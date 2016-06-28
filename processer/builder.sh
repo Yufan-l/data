3eb7b51c0945c6fe07cea1bc12d29f72298aaa5b
Yufan Liu <Yufan2016@users.noreply.github.com>
2016-06-28 11:41:27 -0700

    force download to skip authentication

--- processer/builder.sh
+++ processer/builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'deb http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' > /etc/apt/sources.list.d/ubuntu.list
 sudo apt-get update
-sudo apt-get -d -y install libapr1=1.5.0-1
+sudo apt-get -d -y --force-yes install libapr1=1.5.0-1
 cp /var/cache/apt/archives/libapr1_1.5.0-1_amd64.deb ~/
